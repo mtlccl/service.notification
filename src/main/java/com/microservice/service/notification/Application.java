4494ffd69471cb5941ffb71beb46c1001e4be9f0
mtlccl <matheus.carvalho.lemes@hotmail.com>
2025-03-25 12:32:47 -0300

    docker

--- src/main/java/com/microservice/service/notification/Application.java
+++ src/main/java/com/microservice/service/notification/Application.java
@@ -1,4 +1,4 @@
-package com.mcsb.service.notification;
+package com.microservice.service.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
